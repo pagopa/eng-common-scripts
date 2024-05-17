0211dbc5cd8895ad64b266eed238ad297e3b79bc
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2024-05-17 10:49:20 +0200

    aws init create terraform state

--- aws/create-terraform-state.sh
+++ aws/create-terraform-state.sh
@@ -11,7 +11,7 @@ if [ -z "${AWS_PROFILE}" ]; then
 fi
 
 if [ "$(aws s3api list-buckets --region "${REGION}" --query "Buckets[].Name")" == *"terraform-state"* ]; then
-   echo "[INFO] s3 bucket: ${S3_BUCKET_NAME} already exists"
+   echo "[INFO] s3 bucket: ${S3_BUCKET_NAME} already exists."
 else
    aws s3api create-bucket --bucket "${S3_BUCKET_NAME}" --acl  "private" --create-bucket-configuration LocationConstraint="${REGION}"
    aws s3api put-bucket-versioning --bucket "${S3_BUCKET_NAME}" --versioning-configuration Status=Enabled 
